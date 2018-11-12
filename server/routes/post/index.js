ec18d8913a27ae1283c8928c00de0461e1395af0
Ryan Lucas da Silva Machado <ryangnr123@outlook.com>
2018-11-12 15:55:42 -0200

    12/11

--- server/routes/post/index.js
+++ server/routes/post/index.js
@@ -3,6 +3,8 @@ const clientController = require('../../controllers/clientes');
 const productController = require('../../controllers/produtos');
 const userController = require('../../controllers/user');
 const bodyParser = require('body-parser');
+const checkAuth = require('../../middleware/check-auth')
+
 
 // parse various different custom JSON types as JSON
 routes.use(bodyParser.json())
@@ -15,46 +17,38 @@ routes.use(bodyParser.text({
     type: 'text/html'
 }))
 
-routes.get('/', (req, res) => {
+routes.get('/', checkAuth, (req, res) => {
     res.send('hello world!')
 });
-routes.get('/secret', (req, res) => {
+routes.get('/secret', checkAuth, (req, res) => {
     res.send('hello secret world!')
 });
 
-
-
-routes.post('/api', function (req, res) {
-
-    res.send('dados recebidos ' + JSON.stringify(req.body))
-
-    console.log(JSON.stringify(req.body));
-})
 routes.route('/login')
     .post(userController.login)
 
-routes.route('/api/client/')
-    .get(clientController.allUsers)
-    .post(clientController.newUser)
+routes.route('/client/')
+    .get(checkAuth, clientController.allUsers)
+    .post(checkAuth, clientController.newUser)
 
-routes.route('/api/client/:id')
-    .get(clientController.someUsers)
-    .put(clientController.updateUsers)
-    .delete(clientController.deleteUsers);
+routes.route('/client/:id')
+    .get(checkAuth, clientController.someUsers)
+    .put(checkAuth, clientController.updateUsers)
+    .delete(checkAuth, clientController.deleteUsers);
 
-routes.route('/api/product')
-    .get(productController.allProducts)
-    .post(productController.newProducts)
+routes.route('/product')
+    .get(checkAuth, productController.allProducts)
+    .post(checkAuth, productController.newProducts)
 
-routes.route('/api/product/:id')
-    .get(productController.someProducts)
-    .put(productController.updateProducts)
-    .delete(productController.deleteProducts)
+routes.route('/product/:id')
+    .get(checkAuth, productController.someProducts)
+    .put(checkAuth, productController.updateProducts)
+    .delete(checkAuth, productController.deleteProducts)
 
-routes.route('/api/admin')
-    .get(userController.allUsers)
-    .post(userController.newUser)
-    .delete(userController.deleteUser)
+routes.route('/admin')
+    .get(checkAuth, userController.allUsers)
+    .post(checkAuth, userController.newUser)
+    .delete(checkAuth, userController.deleteUser)
 
 
 module.exports = routes
